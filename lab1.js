f568d6f12bcebae931f2c1e555817addb31fe1a2
benithacyifuzo-ui <benithacyifuzo@gmail.com>
2026-09-18 15:21:50 +0200

    Lab1

---
+++ lab1.js
@@ -0,0 +1,5 @@
+let name = "CYIFUZO benitha";
+let age = 23;
+let town = "Kamonyi";
+let goal = "My goal is to develop my own IT projects and start a technology related business";
+console.log("My name is " + name + ", I am " + age + " years old, I live in " + town + ", and " + goal);
