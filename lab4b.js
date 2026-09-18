a2abcef00014199d1b7d7f58dfc95a8e13d12d1e
benithacyifuzo-ui <benithacyifuzo@gmail.com>
2026-09-18 18:20:11 +0200

    lab4b

---
+++ lab4b.js
@@ -0,0 +1,13 @@
+console.log("\nFizzBuzz from 1 to 100");
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
